1cc53f3d33264e30f8cb76d9226cd596d713ffd4
Keaton Khonsari <keaton.khonsari@gmail.com>
2018-12-10 14:12:04 -0600

    summary

--- kmeans-nytimes.py
+++ kmeans-nytimes.py
@@ -8,6 +8,7 @@ import json
 import os
 from collections import Counter
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from sklearn.tree import DecisionTreeClassifier
@@ -74,9 +75,8 @@ for i in df['lead_paragraph']:
     totalvocab_tokenized.extend(allwords_tokenized)
 
 vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
-#print('there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame')
 
-#define vectorizer parameters
+# Instantiate parameters for vectorizer
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, max_features=10000,
                                  min_df=0.005, stop_words='english',
                                  use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
@@ -91,17 +91,17 @@ km = KMeans(n_clusters=num_clusters)
 km.fit(tfidf_matrix)
 clusters = km.labels_.tolist()
 
+# Dump into pickle file
 joblib.dump(km, 'kmeans-cluster.pkl')
 km = joblib.load('kmeans-cluster.pkl')
-clusters = km.labels_.tolist()
 
+clusters = km.labels_.tolist()
 section_list = df['section'].values.tolist()
 section_cat = { 'section': section_list, 'main_paragraph': df['lead_paragraph'], 'cluster': clusters}
-
 frame = pd.DataFrame(section_cat, index = [clusters] , columns = ['section', 'main_paragraph', 'cluster'])
 
 print("Top terms per cluster:\n")
-#sort cluster centers by proximity to centroid
+# Sort the centers of clusters by proximity to centroid
 order_centroids = km.cluster_centers_.argsort()[:, ::-1]
 
 for i in range(num_clusters):
@@ -109,24 +109,53 @@ for i in range(num_clusters):
     # replace 6 with n words per cluster
     for ind in order_centroids[i, :6]:
         print(' %s' % vocab_frame.loc[terms[ind].split(' ')].values.tolist()[0][0], end=',')
-    print()
-    print()
+    print('\n')
 
     print("Cluster %d section:" % i, end='')
     for section in frame.loc[i]['section'].values.tolist():
         print(' %s,' % section, end='')
-    print()
-    print()
+    print('\n')
 
-print()
-print()
+print('\n')
 
-# Visualization 
+# Visualization
 tfidf_matrix_reduced = TruncatedSVD(n_components=num_clusters, random_state=0).fit_transform(tfidf_matrix)
 tfidf_matrix_embedded = TSNE(n_components=2, perplexity=40, verbose=2).fit_transform(tfidf_matrix_reduced)
 
 fig = plt.figure(figsize = (10, 10))
 ax = plt.axes()
-plt.scatter(tfidf_matrix_embedded[:, 0], tfidf_matrix_embedded[:, 1], marker = ".", c = km.labels_)
-ax.legend()
+
+xs, ys = tfidf_matrix_embedded[:, 0], tfidf_matrix_embedded[:, 1]
+dfd = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=section_list))
+groups = dfd.groupby('label')
+
+# Colors per cluster
+cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+
+# Clusters formed by most frequent terms
+cluster_names = {0: 'Top terms for cluster #0: years, new, time, week, like, president',
+                 1: 'Top terms for cluster #1: january, loving, family, beloved, wife, friends',
+                 2: 'Top terms for cluster #2: new, york, new, city, york, new',
+                 3: 'Top terms for cluster #3: said, states, unit, unit, officials, mr',
+                 4: 'Top terms for cluster #4: editor, letter, jan, dec, pages, susan'}
+for name, group in groups:
+    ax.plot(group.x, group.y, marker='.', linestyle='',
+            label=cluster_names[name], color=cluster_colors[name],
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',
+        which='both',
+        bottom='off',
+        top='off',
+        labelbottom='off')
+    ax.tick_params(\
+        axis= 'y',
+        which='both',
+        left='off',
+        top='off',
+        labelleft='off')
+
+ax.legend(numpoints=1)
 plt.show()
+plt.savefig('kmeans_visual.png', dpi=200)
